a899805415f4f1485250c13282e17dc467ca7db2
SasaFerenc <sasa94sm2@gmail.com>
2017-07-08 21:40:35 +0200

    Log in page initialise.

---
+++ src/main/resources/static/js/Controllers/movies.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Sasa on 08-Jul-17.
+ */
+
+var app = angular.module('movies',[]);
+
+app.controller('movies',['$scope', 'factory', function ($scope, $factory) {
+
+
+
+
+}]);
---
+++ src/main/resources/static/js/Services/factory.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sasa on 08-Jul-17.
+ */
+
+var app = angular.module('factory', []).factory('factory',function ($http) {
+
+    var factory = {};
+
+    return factory;
+});
+
