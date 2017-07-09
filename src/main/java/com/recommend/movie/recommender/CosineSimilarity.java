6b729e19085423c6478a28385a9435eddefbf80c
Predrag <djaps94@gmail.com>
2017-07-09 15:22:04 +0200

    Parallelize matrices tasks

--- src/main/java/com/recommend/movie/recommender/CosineSimilarity.java
+++ src/main/java/com/recommend/movie/recommender/CosineSimilarity.java
@@ -1,30 +1,32 @@
 package com.recommend.movie.recommender;
 
-import com.recommend.movie.model.Genre;
-import com.recommend.movie.model.Movie;
 import com.recommend.movie.repository.GenreRepository;
 import com.recommend.movie.repository.MovieRepository;
 import no.uib.cipr.matrix.DenseMatrix;
-import no.uib.cipr.matrix.MatrixEntry;
 import no.uib.cipr.matrix.sparse.SparseVector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
-import java.util.stream.Collector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class CosineSimilarity {
 
-
     private DenseMatrix movieMatrix;
-    private DenseMatrix userMatrix;
+    private DenseMatrix userSeen;
+    private DenseMatrix userLikes;
     private SparseVector idf;
     private SparseVector userProfile;
+    private SparseVector userProfileSeen;
     private DenseMatrix tfIdf;
+    private ExecutorService exec;
 
     private MovieRepository movieRepository;
     private GenreRepository genreRepository;
@@ -39,86 +41,74 @@ public class CosineSimilarity {
     private void init(){
         long movieRows = movieRepository.count();
         long movieCols = genreRepository.count() + 1;
+        exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         movieMatrix = new DenseMatrix(((Long)movieRows).intValue(), ((Long)movieCols).intValue());
-        userMatrix = new DenseMatrix(((Long)movieRows).intValue(), 1);
-        userMatrix.set(12,0, 1);
+        userSeen = new DenseMatrix(((Long)movieRows).intValue(), 1);
+        userLikes = new DenseMatrix(((Long)movieRows).intValue(), 1);
         idf = new SparseVector(((Long)movieCols).intValue() -1);
-        initialiseMovieMatrix(movieMatrix);
-        normalizeMatrix(movieMatrix);
-        calculateIDF(movieMatrix, idf);
-        userProfile = calculateUserProfile(movieMatrix, userMatrix);
-        tfIdf = calculateTFIDF(idf, movieMatrix);
+        initialiseMovieMatrix(movieMatrix, idf);
     }
 
+    private void initialiseUserLikes(){
 
-    private void initialiseMovieMatrix(DenseMatrix movieMatrix){
-
-        for(int i = 0; i < movieMatrix.numRows(); i++){
-            Optional<Movie> movie = movieRepository.findById(((Integer)i).longValue()+1);
-            if(!movie.isPresent())
-                continue;
-            movieMatrix.set(i, 0, movie.get().getId());
-            for(Genre genre : movie.get().getGenres()){
-                switch(genre.getGenre()){
-                    case "Action" : movieMatrix.set(i, 1, 1); break;
-                    case "Adventure": movieMatrix.set(i, 2, 1); break;
-                    case "Animation": movieMatrix.set(i, 3, 1); break;
-                    case "Childrens": movieMatrix.set(i, 4, 1); break;
-                    case "Comedy": movieMatrix.set(i, 5, 1); break;
-                    case "Crime": movieMatrix.set(i, 6, 1); break;
-                    case "Documentary": movieMatrix.set(i, 7, 1); break;
-                    case "Drama": movieMatrix.set(i, 8, 1); break;
-                    case "Fantasy": movieMatrix.set(i, 9, 1); break;
-                    case "Film-Noir": movieMatrix.set(i, 10, 1); break;
-                    case "Horror": movieMatrix.set(i, 11, 1); break;
-                    case "Musical": movieMatrix.set(i, 12, 1); break;
-                    case "Mystery": movieMatrix.set(i, 13, 1); break;
-                    case "Romance": movieMatrix.set(i, 14, 1); break;
-                    case "Sci-Fi": movieMatrix.set(i, 15, 1); break;
-                    case "Thriller": movieMatrix.set(i, 16, 1); break;
-                    case "War": movieMatrix.set(i, 17, 1); break;
-                    case "Western": movieMatrix.set(i, 18, 1); break;
-                }
-
-            }
-        }
     }
 
-    private void normalizeMatrix(DenseMatrix movieMatrix){
-        for(int i = 0; i < movieMatrix.numRows(); i++){
-            int counter = 0;
-            for(int j = 1; j < movieMatrix.numColumns(); j++){
-                if(movieMatrix.get(i, j) == 1)
-                    counter++;
+    private void initialiseMovieMatrix(DenseMatrix movieMatrix, SparseVector idf){
+        Semaphore s = new Semaphore(0);
+        int i;
+        try {
+            for (i = 0; i < movieMatrix.numRows(); i++) {
+                exec.execute(new InitialiseMatrixTask(i, movieRepository, movieMatrix, s));
             }
-            for(int k = 1; k < movieMatrix.numColumns(); k++){
-                if(movieMatrix.get(i, k) == 1){
-                    movieMatrix.set(i, k , 1/Math.sqrt(counter));
-                }
-            }
+        } finally {
+            exec.shutdown();
         }
+        try {
+            s.acquire(i);
+            normalizeMatrix(movieMatrix, idf);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void normalizeMatrix(DenseMatrix movieMatrix, SparseVector idf){
+        ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        Semaphore s = new Semaphore(0);
+        int i;
+        for(i = 0; i < movieMatrix.numRows(); i++) {
+            exec.execute(new NormalizeMatrixTask(movieMatrix, i, s));
+        }
+        try {
+            s.acquire(i);
+            calculateIDF(movieMatrix, idf);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void calculateIDF(DenseMatrix movieMatrix, SparseVector idf){
-        int numberOfRows = movieMatrix.numRows();
         for(int j = 1; j < movieMatrix.numColumns(); j++){
             int ones = 0;
-            for(int i = 1; i < movieMatrix.numRows(); i++){
+            for(int i = 0; i < movieMatrix.numRows(); i++){
                 if(movieMatrix.get(i, j) > 0)
                     ones++;
             }
             if(ones == 0) {
-                idf.set(j, 0);
+                idf.set(j-1, 0);
                 continue;
             }
-            double result = Math.log10(numberOfRows/ones);
+            double result = Math.log10(movieMatrix.numRows()/ones);
             idf.set(j-1, result);
         }
+        userProfileSeen = calculateUserProfile(movieMatrix, userSeen);
+        userProfile = calculateUserProfile(movieMatrix, userLikes);
+        tfIdf = calculateTFIDF(idf, movieMatrix);
     }
 
     private SparseVector calculateUserProfile(DenseMatrix movieMatrix, DenseMatrix userMatrix){
         SparseVector temp = new SparseVector(movieMatrix.numColumns() -1);
-
         for(int j = 1; j < movieMatrix.numColumns(); j++){
             List<Double> pom = new ArrayList<>();
             for(int k = 0; k < userMatrix.numRows(); k++){
@@ -139,12 +129,12 @@ public class CosineSimilarity {
         return temp;
     }
 
-    public Map<Long, Double> calculatePrediction(){
+    public Map<Long, Double> calculatePredictionSeen(){
         Map<Long, Double> predictions = new TreeMap<>();
         for(int i = 0; i < tfIdf.numRows(); i++) {
             double result = 0;
-            for (int j = 0; j < userProfile.size(); j++) {
-                result += userProfile.get(j) * tfIdf.get(i,j);
+            for (int j = 0; j < userProfileSeen.size(); j++) {
+                result += userProfileSeen.get(j) * tfIdf.get(i,j);
             }
             predictions.put(((Integer)i).longValue()+1, result);
         }
@@ -155,8 +145,33 @@ public class CosineSimilarity {
         return returnMap;
     }
 
-    public SparseVector test(){
-        return userProfile;
+    public Map<Long, Double> calculatePrediction(){
+        Map<Long, Double> predictions = new TreeMap<>();
+        for(int i = 0; i < tfIdf.numRows(); i++) {
+            double result = 0;
+            for (int j = 0; j < userProfile.size(); j++) {
+                result += userProfile.get(j) * tfIdf.get(i,j);
+            }
+            predictions.put(((Integer)i).longValue()+1, result);
+        }
+        Map returnMap = predictions.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (old, newValue) -> old, LinkedHashMap::new));
+
+        return returnMap;
     }
 
+    public void addToSeen(Long movieId){
+        userSeen.set(((Long)movieId).intValue()-1, 0, 1);
+    }
+
+    public void addToProfile(Long movieId){
+        userLikes.set(((Long)movieId).intValue()-1, 0, 1);
+    }
+
+    public void calculateUserSeen(){
+        userProfileSeen = calculateUserProfile(movieMatrix, userSeen);
+    }
+
+    public void calculateUserLikes() { userProfile = calculateUserProfile(movieMatrix, userLikes); }
 }
---
+++ src/main/java/com/recommend/movie/recommender/InitialiseMatrixTask.java
@@ -0,0 +1,94 @@
+package com.recommend.movie.recommender;
+
+
+import com.recommend.movie.model.Genre;
+import com.recommend.movie.model.Movie;
+import com.recommend.movie.repository.MovieRepository;
+import no.uib.cipr.matrix.DenseMatrix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.concurrent.Semaphore;
+
+public class InitialiseMatrixTask implements Runnable {
+
+    private int i;
+    private MovieRepository movieRepository;
+    private DenseMatrix movieMatrix;
+    private Semaphore s;
+
+    public InitialiseMatrixTask(int value, MovieRepository movieRepository, DenseMatrix movieMatrix, Semaphore s){
+        this.i = value;
+        this.movieMatrix = movieMatrix;
+        this.movieRepository = movieRepository;
+        this.s = s;
+    }
+
+    @Override
+    public void run() {
+        Optional<Movie> movie = movieRepository.findById(((Integer) i).longValue() + 1);
+        if (!movie.isPresent())
+            return;
+        movieMatrix.set(i, 0, movie.get().getId());
+        for (Genre genre : movie.get().getGenres()) {
+            switch (genre.getGenre()) {
+                case "Action":
+                    movieMatrix.set(i, 1, 1);
+                    break;
+                case "Adventure":
+                    movieMatrix.set(i, 2, 1);
+                    break;
+                case "Animation":
+                    movieMatrix.set(i, 3, 1);
+                    break;
+                case "Childrens":
+                    movieMatrix.set(i, 4, 1);
+                    break;
+                case "Comedy":
+                    movieMatrix.set(i, 5, 1);
+                    break;
+                case "Crime":
+                    movieMatrix.set(i, 6, 1);
+                    break;
+                case "Documentary":
+                    movieMatrix.set(i, 7, 1);
+                    break;
+                case "Drama":
+                    movieMatrix.set(i, 8, 1);
+                    break;
+                case "Fantasy":
+                    movieMatrix.set(i, 9, 1);
+                    break;
+                case "Film-Noir":
+                    movieMatrix.set(i, 10, 1);
+                    break;
+                case "Horror":
+                    movieMatrix.set(i, 11, 1);
+                    break;
+                case "Musical":
+                    movieMatrix.set(i, 12, 1);
+                    break;
+                case "Mystery":
+                    movieMatrix.set(i, 13, 1);
+                    break;
+                case "Romance":
+                    movieMatrix.set(i, 14, 1);
+                    break;
+                case "Sci-Fi":
+                    movieMatrix.set(i, 15, 1);
+                    break;
+                case "Thriller":
+                    movieMatrix.set(i, 16, 1);
+                    break;
+                case "War":
+                    movieMatrix.set(i, 17, 1);
+                    break;
+                case "Western":
+                    movieMatrix.set(i, 18, 1);
+                    break;
+            }
+        }
+        s.release();
+    }
+}
